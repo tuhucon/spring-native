e7637a88ca8bb0b4e157e8d628aa2c1e5446f05b
Tuan Nguyen <tuan.nguyen1@tyme.com>
2023-01-16 15:53:32 +0700

    init

---
+++ src/test/java/com/example/nativetest/NativetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nativetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativetestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
